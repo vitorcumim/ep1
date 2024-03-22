51a68ea4edd5a5d5d043565d766394f0dcc357e7
vitorcumim <vitorcumim@gmail.com>
2024-03-22 19:24:35 -0300

    escrevi o header da lib sl

---
+++ sl.h
@@ -0,0 +1,24 @@
+#ifndef __SL__
+#define __SL__
+
+#include <stdio.h>
+
+void le_sl(double **m,double *v,int *n);
+
+void criacopia_matriz(double **m,double **mx,int n);
+
+void criacopia_vetor(double *v,double *vx,int n);
+
+void aloca_matriz(double **m,int n);
+
+void aloca_vetor(double *v,int n);
+
+void destroi_matriz(double **m,int n);
+
+void destroi_vetor(double *v,int n);
+
+void imprimeresultado(double *resultado,double *residuo,double tempo,int n);
+
+void destroi_tudo(double **m,double **mx,double *v,double *vx,double* residuo,double *resultado,int n);
+
+#endif
